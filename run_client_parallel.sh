9eb6e4b2af5eec782c7d57f8021a3bdae7a39a9e
cookie <chander92811@gmail.com>
2025-08-18 07:47:23 +0800

    add parallel scripts

---
+++ run_client_parallel.sh
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# +
+# #!/bin/bash
+
+#傳入輪次及client數
+CLIENT=$1
+ROUND=$2
+
+# 參數定義
+EPOCHS=50
+BATCH=16
+WORKERS=4
+
+# cd /home/chander92811/yolov9
+
+
+#權重參數設定
+if [ "$ROUND" -eq 0 ]; then
+  WEIGHTS_ARG="--weights ''"
+else
+  WEIGHTS_ARG="--weights global_round_weights/global_round_${ROUND}.pt"
+fi
+
+# echo "[Client $CLIENT] epochs=$EPOCHS batch=$BATCH workers=$WORKERS"
+
+# 執行client訓練
+TRAIN_CMD="python train_dual.py \
+               --data data/kitti_client${CLIENT}.yaml \
+               ${WEIGHTS_ARG} \
+               --epochs ${EPOCHS} \
+               --batch ${BATCH} \
+               --workers ${WORKERS} \
+               --device 0 \
+               --hyp hyp.scratch-high.yaml \
+               --img 640 \
+               --cfg models/detect/yolov9-c.yaml \
+               --project fed_client_weights \
+               --name client${CLIENT}_r${ROUND}"
+               
+# echo "Executing Training Command:"
+# echo "$TRAIN_CMD"
+# echo "==================="
+$TRAIN_CMD
+
